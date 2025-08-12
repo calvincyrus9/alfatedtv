ed87395576bd67f545a591316343cdf642cc0a26
calvincyrus9 <calvincyrus9@gmail.com>
2025-08-12 16:50:36 +0100

    Initial commit

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,25 +1,6 @@
-// src/config/index.js
-const config = {
-  sitename: "NexusStream",
-  tagline: "The Future of Television Streaming",
-  description: "Experience next-generation IPTV with 10,000+ channels and 50,000+ VOD titles",
-  primaryColor: "#00f3ff",
-  secondaryColor: "#ff00ea",
-  accentColor: "#00ff9d",
-  darkColor: "#0a0a16",
-  darkerColor: "#050510",
-  lightColor: "#e6f7ff",
-  contact: {
-    address: "Neon District, Digital City 10001",
-    phone: "+1 (555) 123-4567",
-    email: "contact@nexusstream.io"
-  },
-  social: {
-    twitter: "#",
-    facebook: "#",
-    instagram: "#",
-    discord: "#"
-  }
+export const siteConfig = {
+  name: "NexusX",
+  description: "Best IPTV service with blazing speed",
+  footerText: "© 2025 NexusX. All rights reserved.",
+  // add more config values used in Pricing, Testimonials, etc.
 };
-
-export default config;
